e13bdfe30e736abc5e95164161f6b7f5d64e6a80
Bhavana M N <bhavanamn29@gmail.com>
2025-05-09 16:41:00 +0530

    Update app.py to host

--- app.py
+++ app.py
@@ -204,9 +204,6 @@ def api_report_data():
     return jsonify(report_data) 
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 if _name_ == '_main_':
     port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port,debug=True)
